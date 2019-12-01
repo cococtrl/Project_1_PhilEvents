a6bde40187af5f978dad098a75912a479d9e209f
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2019-11-30 21:11:17 -0500

    updated API so it filters events to Philadelphia

--- js/script.js
+++ js/script.js
@@ -1,24 +1,52 @@
 //API info
-const promise = $.ajax({url:'https://app.ticketmaster.com/discovery/v2/events.json?apikey=RCh1xsSADGGECDgRe6Ff55bjS3Ab8qgM',
+const promise = $.ajax({url:'https://app.ticketmaster.com/discovery/v2/events.json?city=philadelphia&apikey=RCh1xsSADGGECDgRe6Ff55bjS3Ab8qgM',
 });
+$.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?city=philadelphia&apikey=RCh1xsSADGGECDgRe6Ff55bjS3Ab8qgM",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                // Parse the response.
+                // Do other things.
+             },
+    error: function(xhr, status, err) {
+                // This time, we do not end up here!
+             }
+  }).then(
+      function findEvents(data){
+          console.log(data.events);
+    }
+  );
+
+// functions for date
 let input = document.getElementById("eventDate");
 let submit = document.querySelector('button').addEventListener('click', eventResults);
 
-
 function eventResults(){
-    console.log(input.value)
-    console.log(today)
+    event.preventDefault;
     validDate();
 };
-// let today = new Date();
 let date = new Date();
 let today = date.getFullYear()+'-'+(((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))))+'-'+((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate()));
 
 function validDate(){
     if(input.value >= today){
         console.log('valid')
+        //if date is valid, call function to find events on that date
     } else {console.log("not valid")}
 };
+
+// function to find events on that date
+function eventsFoundOnDate(){
+
+}
+
+//function that itirates through event and keeps ones based on location and adds event to array if it is within location
+
+
+
 //function - when submit button is clicked, find events function is triggered
 // function that decides if that click is valid
 
