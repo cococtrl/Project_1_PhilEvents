64fca2ea3b04436d9ebdf2fb2f609f8b9096befe
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2019-12-02 20:31:01 -0500

    css updates

--- js/script.js
+++ js/script.js
@@ -1,78 +1,51 @@
-// //API info
-// const promise = $.ajax({url:'https://app.ticketmaster.com/discovery/v2/events.json?city=philadelphia&apikey=RCh1xsSADGGECDgRe6Ff55bjS3Ab8qgM',
-// });
-$.ajax({
-    type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?city=philadelphia&startDateTime=2020-08-01T14:00:00Z&apikey=RCh1xsSADGGECDgRe6Ff55bjS3Ab8qgM",
-    async:true,
-    dataType: "json",
-    success: function(json) {
-                console.log(json);
-                // Parse the response.
-                // Do other things.
-             },
-    error: function(xhr, status, err) {
-                // This time, we do not end up here!
-             }
-  }).then(
-      function findEvents(data){
-        // if (date on event === date input){
-        //     console.log (events)
-        // }
-
-          console.log(data._embedded.events);
-          eventList.innerHTML = (data._embedded.events);
-        }
-  );
-
 // functions for date
 let input = document.getElementById("eventDate");
 let submit = document.querySelector('button').addEventListener('click', eventResults);
 let eventList = document.getElementById('results');
+let date = new Date();
+let today = date.getFullYear()+'-'+(((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))))+'-'+((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate()));
 
 function eventResults(){
     event.preventDefault;
     validDate();
-    findEvents();
 };
-let date = new Date();
-let today = date.getFullYear()+'-'+(((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))))+'-'+((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate()));
 
 function validDate(){
     if(input.value >= today){
-        findEvents();
-        //if date is valid, call function to find events on that date
+        console.log(input.value)
     } else {input.value = false;
         alert("Date is not valid. Please select a valid date.")}
 };
 
-// function to find events on that date
-// function eventsFoundOnDate(){
-//     $('#results').html(); //insert date of events on that date
-// }
+//API info
+$.ajax({
+    type:"GET",
+    url:'https://app.ticketmaster.com/discovery/v2/events.json?city=philadelphia&apikey=RCh1xsSADGGECDgRe6Ff55bjS3Ab8qgM',
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+             },
+    error: function(xhr, status, err) {
+             }
+  }).then(
+      function findEvents(data){
+        let eventArray = data._embedded.events
+        let newEventArray = []
+        console.log(eventArray[2])
+        for(i=0, i < eventArray.length; i++){
+            if(eventArray[i].dates.start.localDate == input.value){
+          newEventArray.push(i);
+        eventList.innerHTML = (newEventArray);
+      }
+        //   console.log(data._embedded.events);
+          // eventList.innerHTML = (newEventArray);
+        }
+  );
 
-//function that itirates through event and keeps ones based on location and adds event to array if it is within location
-
-
-
-//function - when submit button is clicked, find events function is triggered
-// function that decides if that click is valid
-
-//function that finds events on that specific date
-    //call function that prefilters to philadelphia
-    //call function that displays events on page
-
-//search function displays events and adds each new event to list
-
-//function that prefilters events to philadelphia
+  //create array to push onto page
+  //create four loop to itirate through events on date 
 
 //function that resets page when home is clicked
-
-
-
-
-
-
 
 
 //Sticky nav functions
@@ -87,3 +60,8 @@ function validDate(){
 //     navbar.classList.remove("sticky");
 //   }
 // };
+        
+//         var i;
+// for (i = 0; i < eventArray.length; i++) {
+//   text += cars[i] + "<br>";
+// }
