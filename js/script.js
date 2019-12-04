7f6b8e400077aacbf3865fcf75a35b7637b066ac
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2019-12-04 18:19:49 -0500

    it's fine

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 let input = document.getElementById("eventDate");
 const submit = document.querySelector('button').addEventListener('click', eventResults);
 let eventList = document.getElementById('results');
-let eventUrl = document.getElementById('link');
 let date = new Date();
 let today = date.getFullYear()+'-'+(((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))))+'-'+((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate()));
 // let toggleEl = getElementById("toggleEl");
@@ -17,7 +16,7 @@ function validDate(){
 function eventResults(event){
     event.preventDefault();
     validDate();
-    
+
 //API info
 // startDateTime=2019-12-12T00:00:00Z&endDateTime=2019-12-14T00:00:00Z&
 $.ajax({
@@ -30,10 +29,13 @@ $.ajax({
         let newEventArray = []
         for(i=0;i < 200; i++) {
             // console.log(input.value)
-            console.log(data._embedded.events[i].dates.start.localDate===input.value)
-        if(data._embedded.events[i].dates.start.localDate == input.value) {
+            let dataStuff = data._embedded.events[i]
+            console.log(dataStuff.dates.start.localDate===input.value)
+        if(dataStuff.dates.start.localDate == input.value) {
+            eventList.style.display = "block"
             let eventArray = [data._embedded.events[0].name + '</br>',           
                             data._embedded.events[0].dates.start.localDate + '</br>'].join("")
+                            eventUrl.href = data._embedded.events[0].url;
             console.log(data._embedded.events[i])
             $(eventList).append(eventArray)
         } else {
